c7c43bffd91b250d981e7a3b8440707dd04b446f
Ls Pain¿ <gcah4451@gmail.com>
2025-09-02 18:05:55 +0100

    Update all

--- converter.js
+++ converter.js
@@ -3,9 +3,12 @@ const gramsOutput = document.getElementById('gramsOutput');
 const kgOutput = document.getElementById('kgOutput');
 const ouncesOutput = document.getElementById('ozOutput');
 
+document.querySelector('.output').style.visibility = 'hidden';
+
 poundsInput.addEventListener('input', calculate);
 
 function calculate(e) {
+    document.querySelector('.output').style.visibility = 'visible';
     let pounds = e.target.value;
     gramsOutput.innerHTML = pounds * 453.592;
     kgOutput.innerHTML = pounds / 2.205;
--- dist/converter.dev.js
+++ dist/converter.dev.js
@@ -4,9 +4,11 @@ var poundsInput = document.getElementById('numberInput');
 var gramsOutput = document.getElementById('gramsOutput');
 var kgOutput = document.getElementById('kgOutput');
 var ouncesOutput = document.getElementById('ozOutput');
+document.querySelector('.output').style.visibility = 'hidden';
 poundsInput.addEventListener('input', calculate);
 
 function calculate(e) {
+  document.querySelector('.output').style.visibility = 'visible';
   var pounds = e.target.value;
   gramsOutput.innerHTML = pounds * 453.592;
   kgOutput.innerHTML = pounds / 2.205;
